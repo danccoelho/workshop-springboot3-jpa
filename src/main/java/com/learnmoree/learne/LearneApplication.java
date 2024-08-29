31399b291fc84033c83d0382536ae75335f1f29b
Daniel Coelho <coelhodaniel06087@gmail.com>
2024-08-29 14:35:51 -0300

    Project created

---
+++ src/main/java/com/learnmoree/learne/LearneApplication.java
@@ -0,0 +1,13 @@
+package com.learnmoree.learne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnmoree/learne/LearneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnmoree.learne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
